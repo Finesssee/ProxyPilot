82f2a5d7be8495aebccc46c6758277eaa0d28c46
Finessse <truongnamphong8@gmail.com>
2025-12-20 00:30:39 +0700

    misc: add reproduction scripts

---
+++ test_gjson.go
@@ -0,0 +1,19 @@
+package main
+import (
+"fmt"
+"github.com/tidwall/gjson"
+)
+func main() {
+json := `{"contents":[{"role":"user"},{"role":"model"}]}`
+fmt.Println("Attempt 1:", gjson.Get(json, "contents.@last.role").String())
+fmt.Println("Attempt 2:", gjson.Get(json, "contents.-1.role").String())
+fmt.Println("Attempt 3:", gjson.Get(json, "contents.#.role").Array())
+    // Get last element from array
+    res := gjson.Get(json, "contents")
+    if res.IsArray() {
+        arr := res.Array()
+        if len(arr) > 0 {
+            fmt.Println("Last explicit:", arr[len(arr)-1].Get("role").String())
+        }
+    }
+}
