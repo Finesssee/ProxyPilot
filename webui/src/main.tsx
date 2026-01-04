5930e6b8c482c3fa5e3e4fd4576e6db85cf372d8
Finessse <truongnamphong8@gmail.com>
2026-01-05 00:55:32 +0700

    fix(webui): load react-grab in dev mode only

--- webui/src/main.tsx
+++ webui/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+// Load react-grab in dev mode only
+if (import.meta.env.DEV) {
+	import("react-grab");
+}
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
